92350f3535a97958637f8527f72b0b0cfecd2bc0
i.atanasov <i.atanasov@CRM-4A004>
2024-02-29 10:23:59 +0000

    MInor style fixes in halls component, style improvements in root component. Code refactoring.

---
+++ backend/variables.js
@@ -0,0 +1,19 @@
+
+const variables = {
+  tobacco: "tobacco-sound-factory",
+  DB_KEY: "mongodb+srv://ivan_johnson:E_Major123456789@cluster0.vzq5zyf.mongodb.net/tobacco-sound-factory?retryWrites=true&w=majority",
+  USER: "ivan_johnson",
+  CRED: "E_Major123456789",
+  MAILTRAPACCESSEMAIL: "info@tsf-bg.com",
+  YAHOOTWOFACTORAUTH: "kqujzpqvhxiwddpr",
+  MYMAIL: 'ivan_johnson2005@yahoo.com',
+  DEV_ENV: "localhost:3000",
+  MAILTRAPDATA: {
+    host: 'live.smtp.mailtrap.io',
+    user: 'api',
+    pass: 'e978725bbbb6c5837599ea831e30233e',
+  },
+  CURRENTPROTOCOL: ''
+}
+
+module.exports = variables;
